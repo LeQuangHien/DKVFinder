e3cac1f769cd533ef554bb93b7ee771440ee3f27
Hien Le <lequanghien247@gmail.com>
2025-06-24 23:02:06 +0200

    Add Retrofit, Arrow, Okhttp logging libs

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -13,6 +13,5 @@ kotlin {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
     implementation(libs.gson)
 }
--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,13 +1,40 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
 }
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+
+android {
+    namespace = "com.hien.le.dkvfinder.core.network"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 24
     }
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    defaultConfig {
+        buildConfigField("String", "API_KEY", "\"eaab6b29-8d8d-4776-88d2-d26c275f50ba\"")
+    }
+}
+
+dependencies {
+    implementation(project(":core:model"))
+
+    implementation(libs.bundles.arrow)
+    implementation(libs.bundles.retrofit)
+    implementation(libs.okhttp.logging.interceptor)
+    implementation(libs.hilt.core)
 }
