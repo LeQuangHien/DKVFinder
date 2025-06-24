5d31fc69d97413010b9ca17552273c715fa49ac9
Hien Le <lequanghien247@gmail.com>
2025-06-24 11:03:37 +0200

    Add Hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -46,6 +48,9 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/hien/le/dkvfinder/DKVFinderApplication.kt
@@ -0,0 +1,8 @@
+package com.hien.le.dkvfinder
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DKVFinderApplication: Application() {
+}
