6001a1c4a5666a8658ad2d0807fdda754f7ebb37
Hien Le <lequanghien247@gmail.com>
2025-06-28 00:06:09 +0200

    Add POI compact model and mappers

---
+++ core/model/src/main/java/com/hien/le/dkvfinder/core/model/data/Mappers.kt
@@ -0,0 +1,29 @@
+package com.hien.le.dkvfinder.core.model.data
+
+import com.hien.le.dkvfinder.core.database.entity.PoiEntity
+
+fun PoiEntity.asDataModel(): PoiCompact {
+    return PoiCompact(
+        id = this.id,
+        title = this.title,
+        address = this.address,
+        town = this.town,
+        telephone = this.telephone,
+        distance = this.distance,
+        distanceUnit = this.distanceUnit,
+        isFavorite = this.isFavorite
+    )
+}
+
+fun PoiCompact.asEntity(): PoiEntity {
+    return PoiEntity(
+        id = this.id ?: 0,
+        title = this.title,
+        address = this.address,
+        town = this.town,
+        telephone = this.telephone,
+        distance = this.distance,
+        distanceUnit = this.distanceUnit,
+        isFavorite = this.isFavorite
+    )
+}
---
+++ core/model/src/main/java/com/hien/le/dkvfinder/core/model/data/PoiCompact.kt
@@ -0,0 +1,33 @@
+package com.hien.le.dkvfinder.core.model.data
+
+data class PoiCompact(
+    val id: Int?,
+    val title: String?,
+    val address: String?,
+    val town: String?,
+    val telephone: String?,
+    val distance: Double?,
+    val distanceUnit: Int?,
+    val isFavorite: Boolean = false
+)
+
+fun Poi.mapToPoiCompact(): PoiCompact {
+    return PoiCompact(
+        id = this.id,
+        title = this.addressInfo?.title,
+        address = formatAddress(this.addressInfo),
+        town = this.addressInfo?.town,
+        telephone = this.addressInfo?.contactTelephone1 ?: this.addressInfo?.contactTelephone2,
+        distance = this.addressInfo?.distance,
+        distanceUnit = this.addressInfo?.distanceUnit
+    )
+}
+
+private fun formatAddress(addressInfo: AddressInfo?): String {
+    return listOfNotNull(
+        addressInfo?.addressLine1,
+        addressInfo?.addressLine2,
+        addressInfo?.postcode,
+        addressInfo?.town
+    ).joinToString(separator = ", ").ifEmpty { "Address not available" }
+}
