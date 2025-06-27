6db945bd62ae9fa1f6a93eb176771bd068a9c499
Hien Le <lequanghien247@gmail.com>
2025-06-28 00:04:35 +0200

    Update `core-model` to be an Android library and add `core-database` dependency

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     implementation(project(":core:model"))
     implementation(project(":core:network"))
     implementation(project(":core:common"))
+    implementation(project(":core:database"))
 
     implementation(libs.bundles.arrow)
     implementation(libs.hilt.android)
