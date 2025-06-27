12bd76cd4518774202fad8a7c3963bad39e40c0c
Hien Le <lequanghien247@gmail.com>
2025-06-28 00:05:36 +0200

    Add connectivity checker

---
+++ core/common/src/main/java/com/hien/le/dkvfinder/core/common/network/ConnectivityChecker.kt
@@ -0,0 +1,33 @@
+package com.hien.le.dkvfinder.core.common.network
+
+import android.Manifest
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import androidx.annotation.RequiresPermission
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+interface ConnectivityChecker {
+    fun isOnline(): Boolean
+}
+
+@Singleton
+class DefaultConnectivityChecker @Inject constructor(
+    @ApplicationContext private val context: Context
+) : ConnectivityChecker {
+    @RequiresPermission(Manifest.permission.ACCESS_NETWORK_STATE)
+    override fun isOnline(): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork ?: return false
+        val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+        return when {
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    }
+}
---
+++ core/common/src/main/java/com/hien/le/dkvfinder/core/common/network/di/CommonModule.kt
@@ -0,0 +1,20 @@
+package com.hien.le.dkvfinder.core.common.di // Or your common DI package
+
+import com.hien.le.dkvfinder.core.common.network.ConnectivityChecker
+import com.hien.le.dkvfinder.core.common.network.DefaultConnectivityChecker
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class CommonModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindConnectivityChecker(
+        impl: DefaultConnectivityChecker
+    ): ConnectivityChecker
+}
