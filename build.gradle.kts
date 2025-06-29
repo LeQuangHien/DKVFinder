e53427d24da9c44f98bbde634f0e6bebf308320b
Hien Le <lequanghien247@gmail.com>
2025-06-29 22:19:38 +0200

    Add Ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,17 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.androidx.navigation.safeargs) apply false
+
+    // Add Ktlint Gradle plugin
+    id("org.jlleitschuh.gradle.ktlint") version "12.3.0" apply false
+}
+
+// Apply Ktlint to all subprojects that have the Kotlin plugin
+subprojects {
+    plugins.withId("org.jetbrains.kotlin.android") { // For Android Kotlin modules
+        apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    }
+    plugins.withId("org.jetbrains.kotlin.jvm") { // For pure Kotlin/JVM modules
+        apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    }
 }
